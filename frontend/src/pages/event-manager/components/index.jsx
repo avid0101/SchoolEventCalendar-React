563a04a7a4da65d9ebeb83e84d501a3ecb59d8c5
avid0101 <justinandrynovela@gmail.com>
2025-12-09 23:53:21 +0800

    refactor(HeaderNav): point export to collocated HeaderNav/

--- frontend/src/pages/event-manager/components/index.jsx
+++ frontend/src/pages/event-manager/components/index.jsx
@@ -3,5 +3,5 @@ export { default as EventForm } from './EventForm';
 export { default as EventTable } from './EventTable';
 export { default as EventModal } from './EventModal';
 export { default as ProfileForm } from './ProfileForm';
-export { default as HeaderNav } from './HeaderNav';
 export { default as MessageAlert } from './MessageAlert';
+export { default as HeaderNav } from '../../components/layout/HeaderNav';
